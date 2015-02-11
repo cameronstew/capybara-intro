fb28067bb30f932f1c62d205cc8f35790c5b2c80
Cameron Webster <cameron.webster@gmail.com>
2015-02-11 14:00:04 -0700

    users can go to location new page

--- spec/features/create_location_spec.rb
+++ spec/features/create_location_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 
-describe 'User can create location' do
+# describe 'User can create location' do
+#
+#   scenario 'User can go to location/new page' do
+#
+#     visit '/'
+#
+#     expect(page).to have_content("Locations")
+#
+#   end
+ describe 'User can create location' do
 
   scenario 'User can go to location/new page' do
 
-    visit '/'
+    visit '/locations'
 
-    expect(page).to have_content("Locations")
+    click_link("New Location")
 
-    click_button 'New Location'
-
-    expect(page).to have_content
+    # expect(page).to have_content "New Location"
 
   end
 
